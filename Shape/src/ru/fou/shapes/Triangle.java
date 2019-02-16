000fb95c00cbffaa39ddaf4014ffaef53a813a5a
Asheli <little.nemo10@gmail.com>
2019-02-17 01:16:40 +0300

    Refactoring

--- Shape/src/ru/fou/shapes/Triangle.java
+++ Shape/src/ru/fou/shapes/Triangle.java
@@ -1,24 +1,31 @@
 package ru.fou.shapes;
 
 public class Triangle implements Shape {
-    private double x1, x2, x3, y1, y2, y3;
+    class Point {
+        private double x;
+        private double y;
 
-    public Triangle(double x1, double x2, double x3, double y1, double y2, double y3) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private Point point1, point2, point3;
+
+    public Triangle(Point point1, Point point2, Point point3) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
     }
 
     public double getWidth() {
-        double width = Math.max(Math.max(x1, x2), x3) - Math.min(Math.min(x1, x2), x3);
+        double width = Math.max(Math.max(point1.x, point2.x), point3.x) - Math.min(Math.min(point1.x, point2.x), point3.x);
         return width;
     }
 
     public double getHeight() {
-        double height = Math.max(Math.max(y1, y2), y3) - Math.min(Math.min(y1, y2), y3);
+        double height = Math.max(Math.max(point1.y, point2.y), point3.y) - Math.min(Math.min(point1.y, point2.y), point3.y);
         return height;
     }
 
@@ -27,9 +34,9 @@ public class Triangle implements Shape {
     }
 
     public double getPerimeter() {
-        double sideLength1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        double sideLength2 = Math.sqrt(Math.pow((x3 - x1), 2) + Math.pow((y3 - y1), 2));
-        double sideLength3 = Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2));
+        double sideLength1 = Math.sqrt(Math.pow((point2.x - point1.x), 2) + Math.pow((point2.y - point1.y), 2));
+        double sideLength2 = Math.sqrt(Math.pow((point3.x - point1.x), 2) + Math.pow((point3.y - point1.y), 2));
+        double sideLength3 = Math.sqrt(Math.pow((point3.x - point2.x), 2) + Math.pow((point3.y - point2.y), 2));
 
         return sideLength1 + sideLength2 + sideLength3;
     }
